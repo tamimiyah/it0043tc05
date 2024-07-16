d83adf2d4c0b1a2946fc20138ebf8996b5b81931
tamimiyah <160693673+tamimiyah@users.noreply.github.com>
2024-07-17 00:14:10 +0800

    Update valera_finalproject.js

--- valera_finalproject.js
+++ valera_finalproject.js
@@ -1,4 +1,13 @@
-
 document.getElementById('sendButton').addEventListener('click', function () {
-  alert('Message Sent!');
+  
+  var name = document.querySelector('input[placeholder="Enter your name"]').value;
+  var email = document.querySelector('input[placeholder="Enter your email"]').value;
+  var message = document.querySelector('input[placeholder="Enter your message"]').value;
+
+  
+  if (name && email && message) {
+    alert('Message Sent!');
+  } else {
+    alert('Please fill out all fields before sending.');
+  }
 });
