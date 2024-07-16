a3bea95d6a8c3ec369b4944c246be07fe2c0f728
tamimiyah <160693673+tamimiyah@users.noreply.github.com>
2024-07-16 08:00:05 +0800

    Add files via upload

--- valera_finalproject.js
+++ valera_finalproject.js
@@ -1,14 +1,4 @@
-$(window).scroll(function() {
-    var top_of_element = $("#Home").offset().top;
-    var bottom_of_element = $("#Home").offset().top + $("#Home").outerHeight();
-    var bottom_of_screen = $(window).scrollTop() + $(window).innerHeight();
-    var top_of_screen = $(window).scrollTop();
 
-    if ((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element)){
-        // the element is visible, add the animation class
-        $("#Home").addClass("animate");
-    } else {
-        // the element is not visible, remove the animation class
-        $("#Home").removeClass("animate");
-    }
+document.getElementById('sendButton').addEventListener('click', function () {
+  alert('Message Sent!');
 });
